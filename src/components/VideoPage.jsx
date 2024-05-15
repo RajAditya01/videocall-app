f2e99ee11cd370b72fa7b66833a5bbf2e9f89029
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-15 19:58:10 +0530

    constant

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,24 +1,47 @@
-// VideoPage.js
-
 import React from 'react';
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const VideoPage = () => {
-  return (
-    <div>
-      <h1>Video Page</h1>
-      <div>
-        {/* Video content goes here */}
-        <video controls>
-          <source src="your_video_source_here.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div>
-        {/* Additional features or related content */}
-        <p>Additional features or related content can be added here.</p>
-      </div>
-    </div>
-  );
+    const { id } = useParams();
+
+    const roomID = getUrlParams().get('roomID') || randomID(5);
+    let myMeeting = async (element) => {
+
+        // generate Kit Token
+        const appID = 1754321524;
+        const serverSecret = "d55de8b440609a91a944e4e912e47c64";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
+
+        // Create instance object from Kit Token.
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        // start the call
+        zp.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Personal link',
+                    url:
+                        window.location.protocol + '//' +
+                        window.location.host + window.location.pathname +
+                        '?roomID=' +
+                        roomID,
+                },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.VideoConference,
+            },
+        });
+    };
+
+    return (
+        <div>
+
+            <div>
+
+            </div>
+        </div>
+    );
 };
 
 export default VideoPage;
