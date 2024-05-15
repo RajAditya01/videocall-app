64d45e8cc3d8caba2288f117ccafbd58e09276df
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-15 19:47:28 +0530

    HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom"
 
 const HomePage = () => {
     const [input , setInput]= useState("");
+    const navigate = useNavigate();
+
+    const submitHandler = () =>{
+        navigate(`/room/${input}`)
+    }
   return (
     <div>
       <div>
-        <input type='text' placeholder='Enter your name...'/>
-        <button>Join</button>
+        <input value={input} onChange={(e)=> setInput(e.target.value)} type='text' placeholder='Enter your name...'/>
+        <button onClick={submitHandler}>Join</button>
       </div>
     </div>
   );
